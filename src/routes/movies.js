b0587c1b4702c147c29e73d01155c6ce44106f4c
Marcelo <marcelosen4@gmail.com>
2020-11-23 16:34:26 -0300

    agregado metodo put, enviando actualizacion desde postman

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -25,6 +25,26 @@ router.post('/', (req, res) => {
     }
 })
 
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, director, year, rating } = req.body;
+
+    if (title && director && year && rating) {
+        _.each(movies, (movie, i) => {
+            if (movie.id == id) {
+                movie.title = title;
+                movie.director = director;
+                movie.year = year;
+                movie.rating = rating;
+            }
+        });
+        res.json(movies)
+    } else {
+        res.status(500).json({error: "There was an error"});
+    }
+
+});
+
 router.delete('/:id', (req, res) => {
     const { id } = req.params;
     _.each(movies, (movie, i) => {
